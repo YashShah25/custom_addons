a0b2dc1846c8f9cf276e03d20f9a86009209763e
‘Odoo <odoo@aktivsoftware.com>
2021-12-03 11:02:19 +0530

    [UPDATE]

--- res_partner/models/sale_order.py
+++ res_partner/models/sale_order.py
@@ -8,34 +8,52 @@ class SaleOrder(models.Model):
 
     @api.model
     def create(self, vals):
-        credit_limit_score = vals.partner_id.credit_limit_score
-        blocking_limit_score = vals.partner_id.blocking_limit_score
-        credit_limit = vals.partner_id.credit_limit
-        blocking_limit = vals.partner_id.blocking_limit
-        amount_total = vals.amount_total
-        email = vals.partner_id.email
+        rtn = super(SaleOrder, self).create(vals)
+        credit_limit_score = rtn.partner_id.credit_limit_score
+        blocking_limit_score = rtn.partner_id.blocking_limit_score
+        credit_limit = rtn.partner_id.credit_limit
+        blocking_limit = rtn.partner_id.blocking_limit
+        amount_total = rtn.amount_total
+        email_to = (
+            self.env["ir.config_parameter"].sudo().get_param("res_partner.param.notify_person_for_limit_exceed"))
         if credit_limit_score and credit_limit:
-            for so in vals.env["sale.order"].search([
-                ("partner_id", "in", vals.partner_id.ids),
+            for so in self.env["sale.order"].search([
+                ("partner_id", "in", rtn.partner_id.ids),
                 ("state", "in", ["draft", "sent"]),
             ]):
                 amount_total += so.amount_total
             if amount_total > credit_limit_score:
-                template = vals.env.ref('res_partner.quotation_limit_mail_template')
-                print("\n\n\n\n\n mail", template)
-                template.send_mail(vals.id, force_send=True)
+                if email_to:
+                    context = {
+                        'message': 'Credit',
+                        'exceeding_amount': credit_limit_score,
+                    }
+                    template_id = self.env.ref('res_partner.quotation_limit_mail_template')
+                    template_id.with_context(context).send_mail(rtn.id, email_values={"email_to": email_to},
+                                                                force_send=True)
+                super(SaleOrder, rtn).unlink()
+                self._cr.commit()
                 raise ValidationError(
                     _('“Your Sale Order Credit Limit has been Exceeded."')
                 )
         if blocking_limit_score and blocking_limit:
-            for total in vals.env["sale.order"].search(
-                    [("partner_id", "in", vals.partner_id.ids), ("state", "=", "sale")]
+            for total in self.env["sale.order"].search(
+                    [("partner_id", "in", rtn.partner_id.ids), ("state", "=", "sale")]
             ):
                 amount_total += total.amount_total
             if amount_total > blocking_limit_score:
-                template = vals.env.ref('res_partner.quotation_limit_mail_template')
-                template.send_mail(vals.id, email_values={'email_to': email})
+                if email_to:
+                    context = {
+                        'message': 'Blocking',
+                        'exceeding_amount': blocking_limit_score,
+                    }
+                    template = self.env.ref('res_partner.quotation_limit_mail_template')
+                    print("\n\nself.id : ", self.id)
+                    template.with_context(context).send_mail(self.id, email_values={"email_to": email_to},
+                                                             force_send=True)
+                super(SaleOrder, rtn).unlink()
+                self._cr.commit()
                 raise ValidationError(
                     _("“You cannot create SO as the Block Limit has been Exceeded”")
                 )
-        return super(SaleOrder, self).create(vals)
+        return rtn
